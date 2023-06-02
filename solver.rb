c48e2accc311cb74a191fa9a38560340153a3ca1
Gracerue <munyaradzi045@gmail.com>
2023-06-02 09:59:46 +0200

    Create fizzbuzz method

--- solver.rb
+++ solver.rb
@@ -1,18 +1,30 @@
 class Solver
-    def factorial(n)
-        raise ArgumentError, 'input should not be a negative integer' if n.negative?
-        results =1
-        if n.zero?
-            results=1
-        else
-        (1...n).each do |int|
-            results *=int
-        end
+  def factorial(n)
+    raise ArgumentError, 'input should not be a negative integer' if n.negative?
+     results =1
+    if n.zero?
+      results=1
+    else
+      (1...n).each do |int|
+      results *=int
     end
-       results
-    end
-    def reverse(string)
-        string.reverse
-    end
+  end
+     results
+  end
 
+  def reverse(string)
+      string.reverse
+  end
+
+  def fizzbuzz(n)
+    if n % 3 == 0 && n % 5 == 0
+        'fizzbuzz'
+    elsif n % 3 == 0
+      'fizz'
+    elsif n % 5 == 0
+      'buzz'
+    else
+      n.to_s
+    end
+  end
 end
