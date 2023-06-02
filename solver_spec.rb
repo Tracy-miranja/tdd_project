445851f7cdabf93509b2b38aa684b8d54883b33e
Tracy-miranja <barazatracy44@gmail.com>
2023-06-02 08:55:11 +0300

    Add test for class

---
+++ solver_spec.rb
@@ -0,0 +1,11 @@
+require_relative './solver.rb'
+require 'rspec'
+
+describe Solver do
+    describe '#factorial' do
+    it 'takes an integer "n" and returns factorial for the number' 
+
+ end
+
+
+end 
