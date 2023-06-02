7c4e10723991317c5dfb61f8d8615954171cfa0a
Tracy-miranja <barazatracy44@gmail.com>
2023-06-02 09:03:31 +0300

    Add a test for factorial method

--- solver_spec.rb
+++ solver_spec.rb
@@ -3,7 +3,11 @@ require 'rspec'
 
 describe Solver do
     describe '#factorial' do
-    it 'takes an integer "n" and returns factorial for the number' 
+    it 'takes an integer "n" and returns factorial of positive integer' do
+        results = Solver.new(n)
+        results.factorial(5)
+        expect(results).to eq(120)
+    end
 
  end
 
