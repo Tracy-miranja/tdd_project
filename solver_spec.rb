fe91344eb017277a9ad5887bc94a1783a675e2f6
Tracy-miranja <barazatracy44@gmail.com>
2023-06-02 09:25:38 +0300

    Add test for argument error

--- solver_spec.rb
+++ solver_spec.rb
@@ -8,6 +8,11 @@ describe Solver do
         results=solver.factorial(5)
         expect(results).to eq(24)
     end
+    it 'retuns an error if the interger is not positive' do
+        solver = Solver.new
+        results = solver.factorial(-1)
+        expect(results).to raise_error(ArgumentError)
+    end
 
  end
 
