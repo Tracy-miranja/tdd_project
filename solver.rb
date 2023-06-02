679f42e89dd6f5941b8f587354999e9558f87bae
Tracy-miranja <barazatracy44@gmail.com>
2023-06-02 09:46:04 +0300

    Add code for the rest

--- solver.rb
+++ solver.rb
@@ -2,9 +2,13 @@ class Solver
     def factorial(n)
         raise ArgumentError, 'input should not be a negative integer' if n.negative?
         results =1
+        if n.zero?
+            results=1
+        else
         (1...n).each do |int|
             results *=int
         end
+    end
        results
     end
 end
