04a3ccac85ba75526d21eeef192dbd5b5939055f
Tracy-miranja <barazatracy44@gmail.com>
2023-06-02 09:17:19 +0300

    Add test for factorial

--- solver.rb
+++ solver.rb
@@ -1,3 +1,9 @@
 class Solver
-    
+    def factorial(n)
+        results =1
+        (1...n).each do |int|
+            results *=int
+        end
+       results
+    end
 end
--- solver_spec.rb
+++ solver_spec.rb
@@ -4,9 +4,9 @@ require 'rspec'
 describe Solver do
     describe '#factorial' do
     it 'takes an integer "n" and returns factorial of positive integer' do
-        results = Solver.new(n)
-        results.factorial(5)
-        expect(results).to eq(120)
+        solver = Solver.new
+        results=solver.factorial(5)
+        expect(results).to eq(24)
     end
 
  end
